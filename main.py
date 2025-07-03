52537904c0e4ad44f02d35f61db1503bd6ce1938
arif7025 <arifmohd2112@gmail.com>
2025-07-03 10:59:37 +0530

    Your commit message

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from flask import Flask
 
+# Initialize the Flask application
 app = Flask(__name__)
 
+# Define a route for the home page
 @app.route('/')
 def hello():
-    return "Hello, World from Render"
+    return "Hello, World from Render!"
 
+# Start the app if the script is run directly
 if __name__ == '__main__':
     app.run(debug=True)
